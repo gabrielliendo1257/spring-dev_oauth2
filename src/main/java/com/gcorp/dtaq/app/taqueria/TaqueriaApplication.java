5192a2a4723bab7580b0ebc171fbcdb3c159bb03
Gabriel L. <gabrielliendo1257@gmail.com>
2026-09-01 22:13:52 -0500

    Initial commit

---
+++ src/main/java/com/gcorp/dtaq/app/taqueria/TaqueriaApplication.java
@@ -0,0 +1,13 @@
+package com.gcorp.dtaq.app.taqueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaqueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaqueriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcorp/dtaq/app/taqueria/TaqueriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcorp.dtaq.app.taqueria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaqueriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
